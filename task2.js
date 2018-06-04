72057b5c19f79c1e86aaba9c5fc110b806d4b898
Behemothhh <elakov111@gmail.com>
2018-06-04 13:25:49 +0500

    изменил алгоритм на более быстрый

--- task2.js
+++ task2.js
@@ -1,12 +1,17 @@
 function fib(num) {
   num = Number.parseInt(num);
-  if (num === 1) {
-    return 1;
-  } else if (num <= 0) {
+  if (num <= 0) {
     return 0;
   }
 
-  return fib(num - 1) + fib(num - 2);
+  const fibArray = [0, 1];
+
+  while(fibArray.length !== num + 1) {
+    const arrLength = fibArray.length;
+    fibArray[arrLength] = fibArray[arrLength - 1] + fibArray[arrLength - 2];
+  }
+
+  return fibArray[fibArray.length - 1];
 }
 
 process.stdout.write(fib(process.argv[2]).toString());
